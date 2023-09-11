4d3f7a0967242394ce322e49eb7ea519d6335e88
dmkur <72811384+dmkur@users.noreply.github.com>
2023-09-11 12:52:17 +0000

    Update axios.service.js

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import {baseURL} from "../constants";
 
+let token = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).authReducer).currentUser
 
-const accessToken = localStorage.getItem("persist:root") === null
+// console.log(token,'LOL1');
+
+const accessToken = token !== null
 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).authReducer).currentUser.accessToken
-: "[]"
+: null
 
+// console.log(accessToken,'LOL2');
 
 const axiosService = axios.create({
     baseURL,
