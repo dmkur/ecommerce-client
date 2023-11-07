2f6ebc1eec3f983c43bbcd1e9404cfefee21fa9d
dmkur <72811384+dmkur@users.noreply.github.com>
2023-11-07 10:31:04 +0000

    Update CartPage.js and cart.slice.js by adding delete action for cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
-import {Footer, Navbar} from "../components";
-import {Add, Remove} from "@mui/icons-material";
-import {mobile} from "../responsive";
-import {useSelector} from "react-redux";
+import { Footer, Navbar } from "../components";
+import { Add, Remove, Clear } from "@mui/icons-material";
+import { mobile } from "../responsive";
+import { useDispatch, useSelector } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
-import {useEffect, useState} from "react";
-import {stripeService} from "../services";
-import {useNavigate} from "react-router"
-import {Link} from "react-router-dom"
-import {constants} from "../constants";
-
+import { useEffect, useState } from "react";
+import { stripeService } from "../services";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import { constants } from "../constants";
+import { cartActions } from "../redux";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 20px;
-  ${mobile({padding: '10px'})}
+  ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
@@ -36,15 +36,15 @@ const TopButton = styled.button`
   font-weight: 600;
   border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) =>
-          props.type === "filled" ? "black" : "transparent"};
+    props.type === "filled" ? "black" : "transparent"};
   color: ${(props) => props.type === "filled" && "white"};
-  &:disabled{
+  &:disabled {
     background-color: grey;
   }
 `;
 
 const TopTexts = styled.div`
-  ${mobile({display: 'none'})}
+  ${mobile({ display: "none" })}
 `;
 const TopText = styled.span`
   text-decoration: underline;
@@ -55,8 +55,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
-  ${mobile({flexDirection: 'column'})}
-
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const Info = styled.div`
@@ -67,7 +66,7 @@ const Product = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 10px;
-  ${mobile({flexDirection: 'column'})}
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const ProductDetail = styled.div`
@@ -116,13 +115,13 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
-  ${mobile({margin: "5px 10px"})}
+  ${mobile({ margin: "5px 10px" })}
 `;
 
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
-  ${mobile({marginBottom: "15px"})}
+  ${mobile({ marginBottom: "15px" })}
 `;
 
 const Hr = styled.hr`
@@ -160,128 +159,139 @@ const Button = styled.button`
   background-color: black;
   color: white;
   font-weight: 600;
-  &:disabled{
+  &:disabled {
     background-color: grey;
   }
 `;
 
 
 const CartPage = () => {
-    const {products, totalPrice} = useSelector(state => state.cartReducer);
-    const {currentUser} = useSelector(state => state.authReducer);
+  const { products, totalPrice } = useSelector((state) => state.cartReducer);
+  console.log(products, "PRODUCTS");
+  const { currentUser } = useSelector((state) => state.authReducer);
+  const dispatch= useDispatch()
 
-    const [stripeToken, setStripeToken] = useState(null);
-    const navigate = useNavigate()
+  const [stripeToken, setStripeToken] = useState(null);
+  const navigate = useNavigate();
 
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
 
-    const onToken = (token) => {
-        setStripeToken(token)
-    }
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const tokenId = stripeToken.id;
+        const amount = totalPrice * 100;
 
+        const { data } = await stripeService.userRequest({ tokenId, amount });
+        navigate("/orders", { state: { data, cart: products } });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken, totalPrice, navigate, products]);
 
+  const deleteCartitem = (id)=>{
+    console.log(id,"deleteCartitem");
+    dispatch(cartActions.deleteProduct(id))
+  }
 
-    useEffect(() => {
-        const makeRequest = async () => {
-            try {
-                const tokenId = stripeToken.id
-                const amount = totalPrice * 100
+  return (
+    <Container>
+      <Navbar />
 
-                const {data} = await stripeService.userRequest({tokenId, amount})
-                navigate("/orders", {state: {data, cart: products}})
-            } catch (e) {
-                console.log(e)
-            }
-        }
-        stripeToken && makeRequest()
-    }, [stripeToken, totalPrice, navigate])
-
-    return (
-        <Container>
-            <Navbar/>
-          
-            <Wrapper>
-                <Title>YOUR BAG</Title>
-                <Top>
-                    <Link to={'/'}><TopButton>CONTINUE SHOPPING</TopButton></Link>
-                    <TopTexts>
-                        <TopText>Shopping Bag(2)</TopText>
-                        <TopText>Your Wishlist (0)</TopText>
-                    </TopTexts>
-                    <TopButton type="filled" disabled={!currentUser}>CHECKOUT NOW</TopButton>
-                </Top>
-                <Bottom>
-                    <Info>
-                        {products && products.map(product => (
-                            <Product key={product._id}>
-                                <ProductDetail>
-                                    <Image
-                                        src={product.img}/>
-                                    <Details>
-                                        <ProductName>
-                                            <b>Product:</b> {product.title}
-                                        </ProductName>
-                                        <ProductId>
-                                            <b>ID:</b> {product._id}
-                                        </ProductId>
-                                        <ProductColor color={product.color}/>
-                                        <ProductSize>
-                                            <b>Size:</b> {product.size}
-                                        </ProductSize>
-                                    </Details>
-                                </ProductDetail>
-                                <PriceDetail>
-                                    <ProductAmountContainer>
-                                        <Add/>
-                                        <ProductAmount>{product.quantity}</ProductAmount>
-                                        <Remove/>
-                                    </ProductAmountContainer>
-                                    <ProductPrice>$ {product.price * product.quantity}</ProductPrice>
-                                </PriceDetail>
-                            </Product>))}
-                        <Hr/>
-
-                    </Info>
-                    <Summary>
-                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
-                        <SummaryItem>
-                            <SummaryItemText>Subtotal</SummaryItemText>
-                            <SummaryItemPrice>$ {totalPrice}</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>Estimated Shipping</SummaryItemText>
-                            <SummaryItemPrice>$ 5.90</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>Shipping Discount</SummaryItemText>
-                            <SummaryItemPrice>$ -5.90</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem type="total">
-                            <SummaryItemText>Total</SummaryItemText>
-                            <SummaryItemPrice>$ {totalPrice}</SummaryItemPrice>
-                        </SummaryItem>
-                        <StripeCheckout
-                            name="DM.SHOP"
-                            // image="https://avatars.githubusercontent.com/u/1486366?v=4"
-                            billingAddress
-                            shippingAddress
-                            description={`Your total is $${totalPrice}`}
-                            amount={totalPrice * 100}
-                            token={onToken}
-                            stripeKey={constants.STRIPE_PUBLIC_KEY}>
-                            <Button
-                                style={{cursor: 'pointer'}}
-                                disabled={!currentUser}
-                            >CHECKOUT NOW!</Button>
-                        </StripeCheckout>
-                        {
-                            !currentUser && <span style={{color:"red"}}>Please sign in for order</span>
-                        }
-                    </Summary>
-                </Bottom>
-            </Wrapper>
-            <Footer/>
-        </Container>
-    )
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <Link to={"/"}>
+            <TopButton>CONTINUE SHOPPING</TopButton>
+          </Link>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled" disabled={!currentUser}>
+            CHECKOUT NOW
+          </TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            {products &&
+              products.map((product) => (
+                <Product key={product._id}>
+                  <ProductDetail>
+                    <Image src={product.img} />
+                    <Details>
+                      <ProductName>
+                        <b>Product:</b> {product.title}
+                      </ProductName>
+                      <ProductId>
+                        <b>ID:</b> {product._id}
+                      </ProductId>
+                      <ProductColor color={product.color} />
+                      <ProductSize>
+                        <b>Size:</b> {product.size}
+                      </ProductSize>
+                    </Details>
+                  </ProductDetail>
+                  <PriceDetail>
+                    <ProductAmountContainer>
+                      <Add />
+                      <ProductAmount>{product.quantity}</ProductAmount>
+                      <Remove />
+                    </ProductAmountContainer>
+                    <ProductPrice>
+                      $ {product.price * product.quantity}
+                    </ProductPrice>
+                  </PriceDetail>
+                  <Clear onClick={()=>deleteCartitem(product._id)}/>
+                </Product>
+              ))}
+            <Hr />
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ {totalPrice}</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ {totalPrice}</SummaryItemPrice>
+            </SummaryItem>
+            <StripeCheckout
+              name="DM.SHOP"
+              // image="https://avatars.githubusercontent.com/u/1486366?v=4"
+              billingAddress
+              shippingAddress
+              description={`Your total is $${totalPrice}`}
+              amount={totalPrice * 100}
+              token={onToken}
+              stripeKey={constants.STRIPE_PUBLIC_KEY}
+            >
+              <Button style={{ cursor: "pointer" }} disabled={!currentUser}>
+                CHECKOUT NOW!
+              </Button>
+            </StripeCheckout>
+            {!currentUser && (
+              <span style={{ color: "red" }}>Please sign in for order</span>
+            )}
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
 };
 
-export {CartPage}
+export { CartPage };
--- src/redux/slices/cart.slice.js
+++ src/redux/slices/cart.slice.js
@@ -1,28 +1,38 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    products: [],
-    cartQuantity: 0,
-    totalPrice: 0
-}
+  products: [],
+  cartQuantity: 0,
+  totalPrice: 0,
+};
 
 const cartSlice = createSlice({
-    name: "cartSlice",
-    initialState,
-    reducers: {
-        addProduct: (state, action) => {
-            state.cartQuantity += 1
-            state.products.push(action.payload)
-            state.totalPrice += action.payload.price * action.payload.quantity
-        }
-    }
+  name: "cartSlice",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      state.cartQuantity += 1;
+      state.products.push(action.payload);
+      state.totalPrice += action.payload.price * action.payload.quantity;
+    },
+    deleteProduct: (state, action) => {      
+      const index = state.products.findIndex(item => item._id === action.payload);
+      state.totalPrice -= state.products[index].price * state.products[index].quantity;
+      state.products.splice(index,1);
+      state.cartQuantity -= 1;
+    
+    },
+  },
 });
 
-const {reducer: cartReducer, actions: {addProduct}} = cartSlice;
+const {
+  reducer: cartReducer,
+  actions: { addProduct, deleteProduct },
+} = cartSlice;
 
 const cartActions = {
-    addProduct
-}
+  addProduct,
+  deleteProduct,
+};
 
-
-export {cartActions, cartReducer}
+export { cartActions, cartReducer };
