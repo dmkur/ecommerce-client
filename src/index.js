f69a148adcc0e4c2647d430eebf5cd00bf5bea0b
dmkur <fgameua@gmail.com>
2023-11-19 19:27:16 +0200

    add basename in index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter><App/></BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}><App/></BrowserRouter>
         </PersistGate>
     </Provider>
 );
