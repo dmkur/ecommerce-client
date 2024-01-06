bb3b735ee875d9a62b0572a5884d5bfc1ea7c240
dmkur <fgameua@gmail.com>
2024-01-06 21:30:25 +0200

    add correct login method for user

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,15 +1,15 @@
 import {axiosService, axiosServiceWithToken} from "./axios.service";
 import {urls} from "../constants";
 
-const _accessToken = 'access'
+const access_token = 'access_token'
 
 const authService = {
     login:(user) => axiosService.post(urls.auth, user),
     getUserById:(id) => axiosServiceWithToken.get(urls.users+`/find/${id}` ),
 
-    setTokens: (access) => localStorage.setItem(_accessToken,access),
-    getAccessToken: () => localStorage.getItem(_accessToken),
-    deleteTokens: () => localStorage.removeItem(_accessToken),
+    setTokens: (access) => localStorage.setItem(access_token,access),
+    getAccessToken: () => localStorage.getItem(access_token),
+    deleteTokens: () => localStorage.removeItem(access_token),
 }
 
 export {authService}
